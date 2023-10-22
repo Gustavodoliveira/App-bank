05fc946431bd44744653dc0e38915adac103fbef
Gustavo <gustavod.oliveira@outlook.com>
2023-10-22 16:55:02 -0300

    feat: initials configs

---
+++ front-end/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import logo from '../../assets/home  21 10.svg';
+
+const Home = () => {
+    return (
+        <>
+            <div>Home</div>
+            <img src={logo} alt="logo" width={300} height={300} />
+        </>
+    );
+};
+
+export default Home;
