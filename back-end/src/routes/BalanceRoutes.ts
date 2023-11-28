32422e9c64176885e0c0865dd4d5de0ed8197c98
Gustvavo de oliveira <gustvavodeoliveira@Gustavodeoliveira>
2023-11-27 21:47:36 -0300

    Feat: route getBalance completed

--- back-end/src/routes/BalanceRoutes.ts
+++ back-end/src/routes/BalanceRoutes.ts
@@ -3,6 +3,7 @@ import  balanceControllerer  from '../controllers/BalanceController';
 import { Router } from 'express';
 const routerBalance = Router();
 
+routerBalance.get('/getBalance', checkUserToken, balanceControllerer.getBalance);
 routerBalance.post('/deposit',checkUserToken, balanceControllerer.getDepositValueBalance);
 
 
