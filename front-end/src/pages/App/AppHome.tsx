255e754f45cfb288bc65ff65c622009e025da341
Gustvavo de oliveira <gustvavodeoliveira@Gustavodeoliveira>
2023-12-03 12:55:14 -0300

    Feat: init modal component

--- front-end/src/pages/App/AppHome.tsx
+++ front-end/src/pages/App/AppHome.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import ModalBalance from '../../components/ModalBalance/ModalBalance';
 
 const AppHome = () => {
     const Authentication = useSelector((state: RootState) => state.isLoggedin);
-    return <div>AppHome</div>;
+    const [openModal, setopenModal] = useState<boolean>(false);
+
+    return (
+        <>
+            <h2>Deposit to have your account activated</h2>
+
+            <button
+                onClick={() =>
+                    openModal ? setopenModal(false) : setopenModal(true)
+                }
+            >
+                Deposit
+            </button>
+            <h2>{openModal}</h2>
+            <ModalBalance isShown={openModal} />
+        </>
+    );
 };
 
 export default AppHome;
