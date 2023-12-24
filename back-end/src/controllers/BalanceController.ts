c7221278f1a0ecf98d3972f2cd1457c9a07b0445
Gustvavo de oliveira <gustvavodeoliveira@Gustavodeoliveira>
2023-12-24 17:52:28 -0300

    Fix: refactoring code

--- back-end/src/controllers/BalanceController.ts
+++ back-end/src/controllers/BalanceController.ts
@@ -8,62 +8,66 @@ import { userModel } from '../models/User';
 
 export default class balanceController {
 
-	static async CreateBalance (req: Request, res: Response) {
-		const balanceValue = req.body;
-		const {userId} = req.body;
+	static async CreateBalance(req: Request, res: Response) {
+		const UserId = req.body;
+		console.log(JSON.stringify(UserId));
 
 
 
 
-		const value = Number.parseInt(balanceValue || 0);
+
+		const value = 10;
 
 
 
 
-		const user = await Users.findOne({where: {id: userId}});
-		const balanceUserId = await balanceModel.findOne({ where: { userId: userId}});
+		const userModel = await Users.findOne({ where: { id: UserId?.id } });
+		const balanceUserId = await balanceModel.findOne({ where: { userId: UserId?.id } });
 
-		if(!user) return res.status(401).json({message: 'you are not user'});
+		if (!userModel) return res.status(401).json({ message: 'you are not user' });
 
 
-		if(balanceUserId) return res.status(401).json({message: 'Account already exists'});
+		if (balanceUserId) return res.status(401).json({ message: 'Account already exists' });
 
 
 
 		try {
 			const Balance = await balanceModel.create({
 				balance: value,
-				userId : id,
+				userId: UserId?.id,
 			});
 
-			return res.status(200).json({message: 'your deposit was made successfully'});
+			return res.status(200).json({ message: 'your deposit was made successfully' });
 		} catch (error) {
-			return res.status(500).json({ message: 'we are having server problems'});
+			return res.status(500).json({ message: 'we are having server problems' });
 
 		}
 
 
 	}
 
-	static async DepositValue(req:Request, res:Response) {
-		const {userId, Amount} = req.body;
+	static async DepositValue(req: Request, res: Response) {
+		const { userId, Amount } = req.body;
 
-		if(!Amount) res.status(400).json({message: 'We need amount value'});
+		if (!Amount) res.status(400).json({ message: 'We need amount value' });
 
-		const userBalance = await balanceModel.findOne({where: {userId: userId}});
+		console.log(userId);
 
-		if(!userBalance) return res.status(400).json({message: 'you don`t have an account'});
+
+		const userBalance = await balanceModel.findOne({ where: { userId: userId } });
+
+		if (!userBalance) return res.status(400).json({ message: 'you don`t have an account' });
 
 		let balanceValue = userBalance?.balance;
 
-		balanceValue = balanceValue + Amount;
+		balanceValue = balanceValue + Number.parseFloat(Amount);
 
 		try {
 			userBalance.update({
 				balance: balanceValue
-			},{where: {userId: userId}});
+			}, { where: { userId: userId } });
 
-			return res.status(200).json({message: 'success deposit value'});
+			return res.status(200).json({ message: 'success deposit value' });
 
 		} catch (error) {
 			console.log(error);
@@ -71,58 +75,58 @@ export default class balanceController {
 		}
 	}
 
-	static async getBalance (req: Request, res:Response) {
+	static async getBalance(req: Request, res: Response) {
 		const token = getToken(req);
 
-		if(!token) return res.status(400).json({message: 'you are not authenticared'});
+		if (!token) return res.status(400).json({ message: 'you are not authenticared' });
 
 		const userId = await getUserByToken(token);
 
-		if(!userId) return;
-		let  valueBalance = 0;
+		if (!userId) return;
+		let valueBalance = 0;
 
-		const idUser = await balanceModel.findOne({where: {userId: userId.id}}) ;
+		const idUser = await balanceModel.findOne({ where: { userId: userId.id } });
 
-		if(!idUser) return res.status(204).json({valueBalance});
+		if (!idUser) return res.status(204).json({ valueBalance });
 
 
 		valueBalance = idUser.balance;
 
 
 
-		return res.status(200).json({valueBalance});
+		return res.status(200).json({ valueBalance });
 
 
 	}
 
 	static async payment(req: Request, res: Response) {
-		const { accountName, value, id} = req.body;
+		const { AccountName, value, id } = req.body;
 
-		const user = await Users.findOne({ where: { id: id}});
+		const user = await Users.findOne({ where: { id: id } });
 
-		if(!user) return res.status(401).json({message: 'user not exists'});
+		if (!user) return res.status(401).json({ message: 'user not exists' });
 
 
-		if(!accountName) return res.status(401).json({ message: 'account name is required to do payment!'});
+		if (!AccountName) return res.status(401).json({ message: 'account name is required to do payment!' });
 
-		if(!value) return res.status(401).json({ message: 'the value is required to do payment!'});
+		if (!value) return res.status(401).json({ message: 'the value is required to do payment!' });
 
-		const balance = await balanceModel.findOne({where: {userId: user.id}});
+		const balance = await balanceModel.findOne({ where: { userId: user.id } });
 
-		if(!balance) return res.status(401).json({message: 'you need to do a deposit '});
+		if (!balance) return res.status(401).json({ message: 'you need to do a deposit ' });
 
-		if(balance.balance < value) return res.status(303).json({message: 'you balance value insufficient'});
+		if (balance.balance < value) return res.status(303).json({ message: 'you balance value insufficient' });
 
-		const newBalance = balance.balance - value;
+		const newBalance = balance.balance - Number.parseFloat(value);
 
 		try {
 			const balanceNew = await balance.update({
 				balance: newBalance,
 			});
 
-			return res.status(200).json({ message: `your new balance ${balanceNew}`});
+			return res.status(200).json({ message: `your new balance ${balanceNew.balance}` });
 		} catch (error) {
-			return res.status(500).json({ message: 'we are having server problems'});
+			return res.status(500).json({ message: 'we are having server problems' });
 
 		}
 
--- back-end/src/middleware/check-token.ts
+++ back-end/src/middleware/check-token.ts
@@ -1,22 +1,28 @@
 import 'dotenv/config';
-import { NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import jwt from 'jsonwebtoken';
 
-
-const checkUserToken = async ( req:Request, res: Response, next:NextFunction ) => {
+const checkUserToken = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
 	const authheader = req.headers.authorization;
 
-	if(!authheader) return res.status(401).json({message: 'you are not authenticated'});
+	if (!authheader)
+		return res.status(401).json({ message: 'you are not authenticated' });
 
 	const token = authheader && authheader.split(' ')[1];
 
-	if(!token) return res.status(401).json({message: 'access denied'});
+	if (!token) return res.status(401).json({ message: 'access denied' });
 
 	try {
 		const verify = jwt.verify(token, `${process.env.jwtSecret}`);
 		req.user = verify;
 		next();
-	} catch (error) { return res.status(400).json({ message: 'Token invalid'});}
+	} catch (error) {
+		return res.status(400).json({ message: 'Token invalid' });
+	}
 };
-export default  checkUserToken;
+export default checkUserToken;
